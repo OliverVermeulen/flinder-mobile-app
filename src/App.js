2101be10ddba91bef69ca9171735d318fd797c9c
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-10-13 14:42:41 +0200

    included react-router-dom and finished routing

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Pricing from "./pages/Pricing";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/pricing":
-      component = <Pricing />;
-      break;
-    case "/about":
-      component = <About />;
-      break;
-  }
   return (
     <>
       <Navbar />
-      <div className="container">{component}</div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,27 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
+      <Link to="/" className="site-title">
         Flinder
-      </a>
+      </Link>
       <ul>
-        <li className="active">
-          <a href="/pricing">Pricing</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
+        <CustomLink to="/pricing">Pricing</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
       </ul>
     </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
