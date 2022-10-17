907498fbad6b69f2f13a1d554cb186c035e2ef5c
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-10-17 13:01:42 +0200

    updating profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,18 @@
 import pfp from "../images/pfp.jpg";
 const Profile = () => {
   return (
+    <>
     <div className="profile-img">
       <img src={pfp} alt="" />
     </div>
+    <div className="profile-info">
+      <div className="name-age">
+      <p>Oliver,</p>
+      <p>20</p>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem laudantium temporibus aut in cupiditate dicta animi neque recusandae totam. Illo </p>
+    </div>
+    </>
   );
 };
 
