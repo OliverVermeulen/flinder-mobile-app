3096eb0a664a94599571eeb0e7c2482232aef7b9
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-10-19 16:37:23 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,46 @@
-import Nav from '../components/Nav'
-import AuthModal from "../components/AuthModel"
-import {useState} from 'react'
-import {useCookies} from "react-cookie"
+import Nav from "../components/Nav";
+import AuthModal from "../components/AuthModel";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
 
 const Home = () => {
-    const [showModal, setShowModal] = useState(false)
-    const [isSignUp, setIsSignUp] = useState(true)
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
-    const authToken = cookies.AuthToken
+  const [showModal, setShowModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const authToken = cookies.AuthToken;
 
-    const handleClick = () => {
-        if (authToken) {
-            removeCookie('UserId', cookies.UserId)
-            removeCookie('AuthToken', cookies.AuthToken)
-            window.location.reload()
-            return
-        }
-        setShowModal(true)
-        setIsSignUp(true)
+  const handleClick = () => {
+    if (authToken) {
+      removeCookie("UserId", cookies.UserId);
+      removeCookie("AuthToken", cookies.AuthToken);
+      window.location.reload();
+      return;
     }
+    setShowModal(true);
+    setIsSignUp(true);
+  };
 
-    return (
-        <div className="overlay">
-            <Nav
-                authToken={authToken}
-                minimal={false}
-                setShowModal={setShowModal}
-                showModal={showModal}
-                setIsSignUp={setIsSignUp}
-            />
-            <div className="home">
-                <h1 class="victory">Swipe Right</h1>
-                <button className="primary-button" onClick={handleClick}>
-                    {authToken ? 'Sign Out' : 'Create Account'}
-                </button>
+  return (
+    <div className="overlay">
+      <Nav
+        authToken={authToken}
+        minimal={false}
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setIsSignUp={setIsSignUp}
+      />
+      <div className="home">
+        <h1 class="neonText">Swipe Right</h1>
 
-                {showModal && (
-                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
-                )}
-            </div>
-        </div>
-    )
-}
-export default Home
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Sign Out" : "Create Account"}
+        </button>
+
+        {showModal && (
+          <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
+        )}
+      </div>
+    </div>
+  );
+};
+export default Home;
