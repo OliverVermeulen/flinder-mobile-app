de0972c4f9b2332d2d4e997ec7b7e89c141e0ae4
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-10-30 17:54:35 +0200

    update

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,17 +1,19 @@
 const Chat = ({ descendingOrderMessages }) => {
   return (
     <>
-      <div className="chat-display">
-        {descendingOrderMessages.map((message, _index) => (
-          <div key={_index}>
-            <div className="chat-message-header">
-              <div className="img-container">
-                <img src={message.img} alt={message.name + " profile"} />
+      <div className="chat-box">
+        <div className="chat-display">
+          {descendingOrderMessages.map((message, _index) => (
+            <div key={_index}>
+              <div className="chat-message-header">
+                <div className="img-container">
+                  <img src={message.img} alt={message.name + " profile"} />
+                </div>
+                <p className="text-message">{message.message}</p>
               </div>
-              <p className="text-message">{message.message}</p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
