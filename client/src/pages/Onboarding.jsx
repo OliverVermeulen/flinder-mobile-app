6726a77b9b441ab113a371e8c4ddfb323045a6f2
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-11-01 15:13:35 +0200

    update

--- client/src/pages/Onboarding.jsx
+++ client/src/pages/Onboarding.jsx
@@ -53,175 +53,180 @@ const Onboarding = () => {
 
   return (
     <>
-      <Nav minimal={true} setShowModal={() => {}} showModal={false} />
-      <div className="onboarding">
-        
-        <form onSubmit={handleSubmit}>
-          <h2 className="neonText">CREATE ACCOUNT</h2>
-          {/* User Name */}
-          <label htmlFor="first_name">Name</label>
-          <input
-            type="text"
-            id="first_name"
-            name="first_name"
-            placeholder="Display Name"
-            required={true}
-            value={formData.first_name}
-            onChange={handleChange}
-          />
-
-          {/* Date Of Birth Inputs */}
-          <label>Date of Birth</label>
-          {/* Day */}
-          <div className="multiple-input-container">
+      <div className="onboarding-overlay">
+        <Nav minimal={true} setShowModal={() => {}} showModal={false} />
+        <div className="onboarding">
+          {/* User Form */}
+          <form onSubmit={handleSubmit}>
+            <h2 className="neonText">CREATE ACCOUNT</h2>
+            {/* User Name */}
+            <label htmlFor="first_name">Name</label>
             <input
-              type="number"
-              id="dob_day"
-              name="dob_day"
-              placeholder="DD"
+              type="text"
+              id="first_name"
+              name="first_name"
+              placeholder="Display Name"
               required={true}
-              value={formData.dob_day}
+              value={formData.first_name}
               onChange={handleChange}
             />
-            {/* Birth Month */}
+
+            {/* Date Of Birth Inputs */}
+            <label>Date of Birth</label>
+            {/* Day */}
+            <div className="multiple-input-container">
+              <input
+                type="number"
+                id="dob_day"
+                name="dob_day"
+                placeholder="DD"
+                required={true}
+                value={formData.dob_day}
+                onChange={handleChange}
+              />
+              {/* Birth Month */}
+              <input
+                type="number"
+                id="dob_month"
+                name="dob_month"
+                placeholder="MM"
+                required={true}
+                value={formData.dob_month}
+                onChange={handleChange}
+              />
+              {/* Birth Year */}
+              <input
+                type="number"
+                id="dob_year"
+                name="dob_year"
+                placeholder="YYYY"
+                required={true}
+                value={formData.dob_year}
+                onChange={handleChange}
+              />
+            </div>
+
+            {/* Gender Choice*/}
+            <label>Gender</label>
+            <div className="multiple-input-container">
+              {/* Man */}
+              <input
+                type="radio"
+                id="man-gender-identity"
+                name="gender_identity"
+                value="man"
+                onChange={handleChange}
+                checked={formData.gender_identity === "man"}
+              />
+              <label htmlFor="man-gender-identity" className="choice-label">
+                M
+              </label>
+              {/* Woman */}
+              <input
+                type="radio"
+                id="woman-gender-identity"
+                name="gender_identity"
+                value="woman"
+                onChange={handleChange}
+                checked={formData.gender_identity === "woman"}
+              />
+              <label htmlFor="woman-gender-identity" className="choice-label">
+                W
+              </label>
+              {/* Non Binary */}
+              <input
+                type="radio"
+                id="more-gender-identity"
+                name="gender_identity"
+                value="more"
+                onChange={handleChange}
+                checked={formData.gender_identity === "more"}
+              />
+              <label htmlFor="more-gender-identity" className="choice-label">
+                NB
+              </label>
+            </div>
+
+            {/* Looking For Choice */}
+            <label>Looking For</label>
+            {/* Men */}
+            <div className="multiple-input-container">
+              <input
+                type="radio"
+                id="man-gender-interest"
+                name="gender_interest"
+                value="man"
+                onChange={handleChange}
+                checked={formData.gender_interest === "man"}
+              />
+              <label htmlFor="man-gender-interest" className="choice-label">
+                M
+              </label>
+              {/* Women */}
+              <input
+                type="radio"
+                id="woman-gender-interest"
+                name="gender_interest"
+                value="woman"
+                onChange={handleChange}
+                checked={formData.gender_interest === "woman"}
+              />
+              <label htmlFor="woman-gender-interest" className="choice-label">
+                W
+              </label>
+              {/* All */}
+              <input
+                type="radio"
+                id="everyone-gender-interest"
+                name="gender_interest"
+                value="everyone"
+                onChange={handleChange}
+                checked={formData.gender_interest === "everyone"}
+              />
+              <label
+                htmlFor="everyone-gender-interest"
+                className="choice-label"
+              >
+                All
+              </label>
+            </div>
+
+            {/* Profile Picture */}
+            <label htmlFor="url">Profile Picture</label>
             <input
-              type="number"
-              id="dob_month"
-              name="dob_month"
-              placeholder="MM"
+              type="url"
+              id="url"
+              name="url"
               required={true}
-              value={formData.dob_month}
+              placeholder="Photo URL"
               onChange={handleChange}
             />
-            {/* Birth Year */}
+            {/* Picture Display */}
+            <div className="photo-container">
+              {formData.url && (
+                <img src={formData.url} alt="profile pic preview" />
+              )}
+            </div>
+
+            {/* <input type="file" name="sampleFile" /> */}
+
+            {/* About Me */}
+            <label htmlFor="about">About Me</label>
             <input
-              type="number"
-              id="dob_year"
-              name="dob_year"
-              placeholder="YYYY"
+              type="text"
+              id="about"
+              name="about"
               required={true}
-              value={formData.dob_year}
+              placeholder="I like long walks..."
+              value={formData.about}
               onChange={handleChange}
+              minLength="10"
+              maxLength="150"
             />
-          </div>
 
-          {/* Gender Choice*/}
-          <label>Gender</label>
-          <div className="multiple-input-container">
-            {/* Man */}
-            <input
-              type="radio"
-              id="man-gender-identity"
-              name="gender_identity"
-              value="man"
-              onChange={handleChange}
-              checked={formData.gender_identity === "man"}
-            />
-            <label htmlFor="man-gender-identity" className="choice-label">
-              M
-            </label>
-            {/* Woman */}
-            <input
-              type="radio"
-              id="woman-gender-identity"
-              name="gender_identity"
-              value="woman"
-              onChange={handleChange}
-              checked={formData.gender_identity === "woman"}
-            />
-            <label htmlFor="woman-gender-identity" className="choice-label">
-              W
-            </label>
-            {/* Non Binary */}
-            <input
-              type="radio"
-              id="more-gender-identity"
-              name="gender_identity"
-              value="more"
-              onChange={handleChange}
-              checked={formData.gender_identity === "more"}
-            />
-            <label htmlFor="more-gender-identity" className="choice-label">
-              NB
-            </label>
-          </div>
-
-          {/* Looking For Choice */}
-          <label>Looking For</label>
-          {/* Men */}
-          <div className="multiple-input-container">
-            <input
-              type="radio"
-              id="man-gender-interest"
-              name="gender_interest"
-              value="man"
-              onChange={handleChange}
-              checked={formData.gender_interest === "man"}
-            />
-            <label htmlFor="man-gender-interest" className="choice-label">
-              M
-            </label>
-            {/* Women */}
-            <input
-              type="radio"
-              id="woman-gender-interest"
-              name="gender_interest"
-              value="woman"
-              onChange={handleChange}
-              checked={formData.gender_interest === "woman"}
-            />
-            <label htmlFor="woman-gender-interest" className="choice-label">
-              W
-            </label>
-            {/* All */}
-            <input
-              type="radio"
-              id="everyone-gender-interest"
-              name="gender_interest"
-              value="everyone"
-              onChange={handleChange}
-              checked={formData.gender_interest === "everyone"}
-            />
-            <label htmlFor="everyone-gender-interest" className="choice-label">
-              All
-            </label>
-          </div>
-
-          {/* Profile Picture */}
-          <label htmlFor="url">Profile Picture</label>
-          <input
-            type="url"
-            id="url"
-            name="url"
-            required={true}
-            placeholder="Photo URL"
-            onChange={handleChange}
-          />
-          {/* Picture Display */}
-          <div className="photo-container">
-            {formData.url && (
-              <img src={formData.url} alt="profile pic preview" />
-            )}
-          </div>
-
-          {/* <input type="file" name="sampleFile" /> */}
-
-          {/* About Me */}
-          <label htmlFor="about">About Me</label>
-          <input
-            type="text"
-            id="about"
-            name="about"
-            required={true}
-            placeholder="I like long walks..."
-            value={formData.about}
-            onChange={handleChange}
-            minLength="10"
-            maxLength="150"
-          />
-
-          <input type="submit" />
-        </form>
+            <input type="submit" />
+          </form>
+        </div>
       </div>
     </>
   );
